73155425147c63481ce72d5a633df4308de71431
Megan <dommelmeg@gmail.com>
2022-10-16 14:33:13 -0400

    commit tree 66e72641df35ff3463947a0b68e214e08d4bc728

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 400 && feet < 2000) {
+    result = "That will be twenty bucks.";
+  } else if (feet >= 2000 && feet < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet >= 2500) {
+    result = "No can do.";
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    default:
+      message = "Bye.";
+  }
+  return message
 }
